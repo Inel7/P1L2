aab49984b38286a225b23f25488162a89c3bf55e
Inel7 <indyelens@gmail.com>
2024-09-28 10:30:00 +0200

    Add files via upload

---
+++ naam en leeftijd.py
@@ -0,0 +1,5 @@
+def vraag_naam_en_leeftijd():
+    naam=input("wat is jouw naam? ")
+    leeftijd=input("wat is jouw leeftijd? ")
+    print("je naam is", naam, "en je leeftijd is", leeftijd)
+vraag_naam_en_leeftijd()
