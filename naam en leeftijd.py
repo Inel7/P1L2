9cf73818c717aed709ee4141e5b64ea6725b69b5
Inel7 <indyelens@gmail.com>
2024-09-28 10:45:10 +0200

    Update naam en leeftijd.py

--- naam en leeftijd.py
+++ naam en leeftijd.py
@@ -3,3 +3,9 @@ def vraag_naam_en_leeftijd():
     leeftijd=input("wat is jouw leeftijd? ")
     print("je naam is", naam, "en je leeftijd is", leeftijd)
 vraag_naam_en_leeftijd()
+
+def vraag_input(onderwerp):
+    return input("geef me een",onderwerp)
+def print_verhaal(naam,plaats,dier):
+    print("Er was eens een kindje genaamd", naam,". Op een dag besloot Jelle Joligheid om een spannend avontuur te beleven in",plaats,".")
+    print("Samen met",naam,"'s trouwe metgezel, een",dier,"genaamd Fluffy, beleefden ze geweldige avonturen en creëerden ze onvergetelijke herinneringen.")
